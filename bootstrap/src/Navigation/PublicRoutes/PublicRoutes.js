2a34aa9abe54d255a26798cfbd3170beedf59811
tejesh123 <kota.t@exalogic.co>
2023-04-14 13:32:59 +0530

    UI

--- bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
+++ bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
@@ -5,15 +5,15 @@ import {
 } from 'react-router-dom';
 //  imported pages here
 import Home from '../../pages/Home/Home';
-
-
+import SignIn from '../../pages/Auth/SignIn/SignIn';
 
 const PublicRoutes = () => {
     return ( 
         <>
         <Router>
             <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
+                <Route exact path='/auth/login' element={<SignIn />}></Route>
+                <Route exact path='/' element={< Home />}></Route>
             </Routes>
         </Router>
         </>
---
+++ bootstrap/src/pages/Auth/SignIn/SignIn.js
@@ -0,0 +1,19 @@
+import {
+    Container,
+    Row, 
+    Col
+} from 'react-bootstrap';
+
+const SignIn = () => {
+    return ( 
+        <>
+            <Container fluid className="px-full py-0 mx-0">
+                <Row className='min-vh-100 justify-content-center'>
+                            
+                </Row>
+            </Container>
+        </>
+    );
+}
+ 
+export default SignIn;
