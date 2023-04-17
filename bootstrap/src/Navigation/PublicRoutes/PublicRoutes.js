677974eabb7fc50ba16fa2d0329bba41b93a5d51
tejesh123 <kota.t@exalogic.co>
2023-04-17 09:28:52 +0530

    UI changes modal component

--- bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
+++ bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
@@ -8,6 +8,7 @@ import Home from '../../pages/Home/Home';
 import SignIn from '../../pages/Auth/SignIn/SignIn';
 import SignUp from '../../pages/Auth/SignUp/SignUp';
 import ForgotPassword from '../../pages/Auth/ForgotPassword/ForgotPassword';
+import Modal from '../../pages/Components/Modal/Modal';
 
 const PublicRoutes = () => {
     return ( 
@@ -17,6 +18,7 @@ const PublicRoutes = () => {
                 <Route exact path='/auth/login' element={<SignIn />}></Route>
                 <Route exact path='/auth/register' element={<SignUp />}></Route>
                 <Route exact path='/auth/forgot-password' element={<ForgotPassword />}></Route>
+                <Route exact path='/components/modal' element={<Modal />}></Route>
                 <Route exact path='/' element={< Home />}></Route>
             </Routes>
         </Router>
---
+++ bootstrap/src/pages/Components/Modal/BootstrapModal/BootstrapModal.js
@@ -0,0 +1,17 @@
+import {
+    Row, 
+    Col,
+    Button
+} from 'react-bootstrap';
+
+const BootstrapModal = () => {
+    return ( 
+        <>
+            <Button variant="primary" type="button" className="text-uppercase font-12 py-2 px-4 d-inline-block rounded">
+                Submit
+            </Button>
+        </>
+    );
+}
+ 
+export default BootstrapModal;
