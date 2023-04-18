cf8b46af76afadc4ef1254385a1a631cff0dfc9f
mari_joseph <veena.j@exalogic.co>
2023-04-18 23:25:54 +0530

    Layout structure

--- bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
+++ bootstrap/src/Navigation/PublicRoutes/PublicRoutes.js
@@ -1,7 +1,7 @@
 import {
-	BrowserRouter as Router,
 	Routes,
-	Route
+	Route,
+    useLocation
 } from 'react-router-dom';
 //  imported pages here
 import Home from '../../pages/Home/Home';
@@ -14,24 +14,44 @@ import {ChartsWrapper} from '../../pages/Charts/ChartsWrapper';
 import {GanttChartWrapper} from'../../pages/Charts/GanttChart/GanttChartWrapper';
 import {DxSchedular} from "../../pages/Charts/DxSchedular/DxSchedular";
 import AlertBootstrap from '../../pages/Components/Alert/AlertBootstrap';
+import Header from '../../pages/Layout/Header/Header';
+import Sidebar from '../../pages/Layout/Sidebar/Sidebar';
 
 const PublicRoutes = () => {
+    let location = useLocation();
     return ( 
         <>
-        <Router>
+        {/* Before Login */}
+        {(location.pathname === '/auth/login') || (location.pathname === '/auth/forgot-password') || (location.pathname === '/auth/register') ?
+        (
             <Routes>
                 <Route exact path='/auth/login' element={<SignIn />}></Route>
                 <Route exact path='/auth/register' element={<SignUp />}></Route>
                 <Route exact path='/auth/forgot-password' element={<ForgotPassword />}></Route>
-                <Route exact path='/components/modal' element={<ModalPopup />}></Route>
-                <Route exact path='/components/accordion' element={<AccordionBootstrap />}></Route>
-                <Route exact path='/components/alert' element={<AlertBootstrap />}></Route>
-                <Route exact path='/' element={<Home />}></Route>
-                <Route exact path='/charts' element={<ChartsWrapper />}></Route>
-                <Route exact path='/gantt-chart' element={<GanttChartWrapper />}></Route> 
-                <Route exact path='/dx-schedular' element={<DxSchedular />}></Route>
             </Routes>
-        </Router>
+        ):
+         (
+            <div className='Wrapper'>
+                <Sidebar />
+                <div className="ContentWrapper">
+                    <div className="Content">
+                        <Header/>
+                        <div className='container-fluid'>
+                            <Routes>
+                                <Route exact path='/components/modal' element={<ModalPopup />}></Route>
+                                <Route exact path='/components/accordion' element={<AccordionBootstrap />}></Route>
+                                <Route exact path='/components/alert' element={<AlertBootstrap />}></Route>
+                                <Route exact path='/dashboard' element={<Home />}></Route>
+                                <Route exact path='/charts' element={<ChartsWrapper />}></Route>
+                                <Route exact path='/gantt-chart' element={<GanttChartWrapper />}></Route> 
+                                <Route exact path='/dx-schedular' element={<DxSchedular />}></Route>
+                            </Routes>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}
+        
         </>
      );
 }
---
+++ bootstrap/src/pages/Layout/NavMenu/NavMenu.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Nav, Collapse } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Scheduler } from '@aldabil/react-scheduler';
+
+const NavMenu = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Nav
+        activeKey="/"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        className="flex-column"
+      >
+        <Nav.Item>
+          <Link to={"/"} activeClassName="active">Dashboard</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to={" "} className='nav-drop' onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open}>Components</Link>
+          <Collapse in={open}>
+            <div id="example-collapse-text">
+              <Link to={"/components/modal"}>Modal</Link>
+              <Link to={"/components/accordion"}>Accordian</Link>
+              <Link to={"/components/alert"}>Alert</Link>
+              <Link to={"/charts"}>Charts</Link>
+              <Link to={"/gantt-chart"}>Gantt Chart</Link>
+              <Link to={"/dx-schedular"}>DX Scheduler</Link>
+            </div>
+          </Collapse>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+}
+
+export default NavMenu;
