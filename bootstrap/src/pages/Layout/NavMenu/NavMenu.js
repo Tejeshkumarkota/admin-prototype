b292eb029966e40671bcf6f578f0a00fc0ec17bb
mari_joseph <veena.j@exalogic.co>
2023-04-19 15:19:14 +0530

    ui

--- bootstrap/src/pages/Layout/NavMenu/NavMenu.js
+++ bootstrap/src/pages/Layout/NavMenu/NavMenu.js
@@ -23,9 +23,30 @@ const NavMenu = () => {
               <Link to={"/components/modal"}>Modal</Link>
               <Link to={"/components/accordion"}>Accordian</Link>
               <Link to={"/components/alert"}>Alert</Link>
-              <Link to={"/charts"}>Charts</Link>
+            </div>
+          </Collapse>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to={" "} className='nav-drop' onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text1"
+            aria-expanded={open}>Charts</Link>
+          <Collapse in={open}>
+            <div id="example-collapse-text1">
+              <Link to={"/charts"}>Chart Js</Link>
+              <Link to={""}>Apex Chart</Link>
               <Link to={"/gantt-chart"}>Gantt Chart</Link>
               <Link to={"/dx-schedular"}>DX Scheduler</Link>
+              <Link to={"/floor-plan-admin"}>Floor Plan Admin</Link>
+            </div>
+          </Collapse>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to={" "} className='nav-drop' onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text2"
+            aria-expanded={open}>Floor Plan</Link>
+          <Collapse in={open}>
+            <div id="example-collapse-text2">
+              <Link to={"/floor-plan-admin"}>Floor Plan Admin</Link>
             </div>
           </Collapse>
         </Nav.Item>
